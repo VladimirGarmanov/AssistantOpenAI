c873dc703cafbef0752cd9441cce44fd3357f514
VladimirGarmanov <v.garmanov@sch-int.ru>
2024-01-27 01:00:51 +0300

    fast code

--- main.py
+++ main.py
@@ -1,59 +1,88 @@
+import time
 import asyncio
+import sqlite3
 from aiogram import Bot, Dispatcher, types, executor
-from aiogram.types import ChatActions
 import openai
+client = openai.OpenAI(api_key="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")
+Assistant_ID = 'asst_FHM2vXbTHBZdBQUQ4wbPipwK'
+TELEGRAM_TOKEN = '6671314480:AAGmeTFRxedPPYR4782fkkqTDfNVPhebSWw'
 
-# Замените на ваш токен Telegram бота
-TELEGRAM_TOKEN = '6787450167:AAGByC55w7mBxObi0hK7XWh29NOqgBMTqEs' # Здесь вы должны указать токен вашего Telegram бота.
 
-# Замените на ваш API ключ от OpenAI
-OPENAI_API_KEY = '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'
-# Здесь вы должны указать свой API ключ от OpenAI.
+bot = Bot(token=TELEGRAM_TOKEN)
+dp = Dispatcher(bot)
 
-# Инициализация бота и диспетчера
-bot = Bot(token=TELEGRAM_TOKEN) # Создание экземпляра бота с указанным токеном.
-dp = Dispatcher(bot) # Создание диспетчера для управления обработчиками сообщений.
 
-# Глобальный объект для хранения сессий чата
-chat_sessions = {} # Словарь для хранения истории сообщений каждого пользователя.
+conn = sqlite3.connect('users.db')
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+    chat_id INTEGER PRIMARY KEY,
+    thread TEXT
+)''')
+conn.commit()
 
-# Функция для создания запроса
-def create_prompt(user_id):
-    messages = chat_sessions[user_id]['messages'] # Получение списка сообщений пользователя.
-    prompt = [
-        {"role": "system", "content": "Я менеджер компании, Моя главная задача помогать людям"}
-    ] # Создание системного сообщения для установки контекста диалога.
-    prompt.extend(messages) # Добавление всех предыдущих сообщений пользователя в запрос.
-    return prompt # Возврат сформированного запроса.
+# Функция для добавления пользователя в базу данных
+async def handle_with_assistant(message, chat_id):
+    print('генерация началась')
+    cursor.execute('SELECT thread FROM users WHERE chat_id = ?', (chat_id,))
+    result = cursor.fetchone()
+    thread_id = result[0] if result is not None else add_user(chat_id)
+    message_answer = client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=message.text
 
-# Функция обработки чата с GPT
-async def handle_chat_with_gpt(user_id, message):
-    if user_id not in chat_sessions:
-        chat_sessions[user_id] = {'messages': []} # Если пользователя нет в сессиях, создается новая сессия.
+    )
+    run = client.beta.threads.runs.create(
+        thread_id=thread_id,
+        assistant_id=Assistant_ID,
+        instructions=f"Be Pleasant."
+    )
 
-    chat_sessions[user_id]['messages'].append({'role': 'user', 'content': message}) # Добавление сообщения пользователя в историю.
 
-    # Имитация набора текста
-    await bot.send_chat_action(user_id, ChatActions.TYPING) # Отправка действия "печатает" пользователю.
+    time.sleep(10)
+    run_status = client.beta.threads.runs.retrieve(
+        thread_id=thread_id,
+        run_id=run.id
+    )
 
-    prompt = create_prompt(user_id) # Создание запроса для модели.
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        api_key=OPENAI_API_KEY,
-        messages=prompt
-    ) # Отправка запроса в модель ChatGPT и получение ответа.
+    print(run_status.status)
+    while run_status.status == 'in_progress':
+        time.sleep(5)
+        run_status = client.beta.threads.runs.retrieve(
+            thread_id=thread_id,
+            run_id=run.id
+        )
+    print(run_status.status)
+    if run_status.status == 'completed':
+        messages = client.beta.threads.messages.list(
+            thread_id=thread_id
+        )
 
-    chat_sessions[user_id]['messages'].append(
-        {'role': 'assistant', 'content': response['choices'][0]['message']['content']}) # Добавление ответа модели в историю чата.
+        msg=messages.data[0]
+        role = msg.role
+        content = msg.content[0].text.value
+        print(f"{role.capitalize()}: {content}")
+        await bot.send_message(chat_id=message.chat.id, text=content)
 
-    await bot.send_message(user_id, response['choices'][0]['message']['content']) # Отправка ответа пользователю.
+def add_user(chat_id):
+    cursor.execute('SELECT chat_id FROM users WHERE chat_id = ?', (chat_id,))
+    thread = client.beta.threads.create()
+    if cursor.fetchone() is None:
+        cursor.execute('INSERT INTO users (chat_id, thread) VALUES (?, ?)', (chat_id, thread.id))
+        conn.commit()
+    return thread.id
+async def answer_user(message_response,message):
+    await message.answer(message_response)
 
-# Обработчик текстовых сообщений
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    add_user(message.chat.id)
+    await message.answer("Привет! Вы зарегистрированы.")
 @dp.message_handler(content_types=types.ContentTypes.TEXT)
-async def text_handler(message: types.Message):
-    await handle_chat_with_gpt(message.from_user.id, message.text) # Обработка текстовых сообщений от пользователя.
+async def echo_message(message: types.Message):
 
+    await handle_with_assistant(message, message.chat.id)
 # Запуск бота
 if __name__ == '__main__':
-    openai.api_key = OPENAI_API_KEY # Установка API ключа OpenAI.
-    executor.start_polling(dp, skip_updates=True) # Запуск бота для прослушивания входящих сообщений.
+    executor.start_polling(dp, skip_updates=True)
+
